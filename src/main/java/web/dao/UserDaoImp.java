4434ffca4b1019970b00015debcf54da3ae959ab
marsulin <139697217+marsulin@users.noreply.github.com>
2023-12-14 13:20:22 +0300

    Add files via upload

---
+++ src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,39 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getUsersList() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public User getUser(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+
+    @Override
+    public void editUser(User user) {
+        entityManager.merge(user);
+    }
+}
---
+++ src/main/java/web/service/UserServiceImp.java
@@ -0,0 +1,47 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService{
+
+    private final UserDao userDao;
+
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getUsersList() {
+        return userDao.getUsersList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User getUser(int id) {
+        return userDao.getUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(int id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void editUser(User user) {
+        userDao.editUser(user);
+    }
+}
